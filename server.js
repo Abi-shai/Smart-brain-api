df119e5fc775b3969217113904cb350136e97d3f
Abishai Yapélé <abishaiyp@gmail.com>
2022-04-17 15:32:34 +0000

    Adding another route⬆️

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use(express.json())
 app.use(cors())
 
 
+app.post('/', (req, res) => {res.json(`Hey it's working`)})
+
 // Route config for the signin entrie point
 app.post('/signin', (req, res) => {signIn.handleSignIn(req, res, database, bcrypt)})
 
